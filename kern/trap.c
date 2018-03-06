615182a77470eb01040fb88a3fa1de30b12ed050
ming <minghust>
2018-03-06 20:27:02 +0800

    add user-mode page fault handler

--- kern/trap.c
+++ kern/trap.c
@@ -381,8 +381,35 @@ page_fault_handler(struct Trapframe *tf)
 	// LAB 4: Your code here.
 
 	// Destroy the environment that caused the fault.
-	cprintf("[%08x] user fault va %08x ip %08x\n",
-		curenv->env_id, fault_va, tf->tf_eip);
-	print_trapframe(tf);
-	env_destroy(curenv);
+
+	// no handler, destroy itself
+	if(!curenv->env_pgfault_upcall){
+		cprintf("[%08x] user fault va %08x ip %08x\n",
+			curenv->env_id, fault_va, tf->tf_eip);
+		print_trapframe(tf);
+		env_destroy(curenv);
+		return;
+	}
+	struct UTrapframe *utf;
+
+	// already running on the exception stack
+	if(tf->tf_esp < UXSTACKTOP && tf->tf_esp >= UXSTACKTOP-PGSIZE)
+		utf = (struct UTrapframe *)(tf->tf_esp - 4 - sizeof(struct UTrapframe));
+	else
+		utf = (struct UTrapframe *)(UXSTACKTOP - sizeof(struct UTrapframe));
+
+	// set user stack
+	utf->utf_fault_va = fault_va;
+	utf->utf_err = tf->tf_err;
+	utf->utf_regs = tf->tf_regs;
+	utf->utf_eip = tf->tf_eip;
+	utf->utf_eflags = tf->tf_eflags;
+	utf->utf_esp = tf->tf_esp;
+
+	curenv->env_tf.tf_eip = (uintptr_t)curenv->env_pgfault_upcall;
+	curenv->env_tf.tf_esp = (uintptr_t)utf;
+
+	env_run(curenv);
+	return;
+
 }
